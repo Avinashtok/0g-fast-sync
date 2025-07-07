21de3bd6eeccc3ebd5e784384481a136f79b0ad0
Avinash Kumar <avinashtok@gmail.com>
2025-07-07 23:48:55 +0000

    Fix wget command in restore script

--- restore_3507655.sh
+++ restore_3507655.sh
@@ -12,7 +12,7 @@ echo ""
 echo "Download and extract the Flow db:"
 wget https://github.com/Avinashtok/0g-fast-sync/releases/download/backup-3507655/flow_db-3507655.tar.gz \
   -O $HOME/0g-storage-node/run/db/flow_db.tar.gz && \
-  tar -xzvf $HOME/0g-storage-node/run/db/flow_db.tar.gz -C $HOME/0g-storage-node/run/db/
+tar -xzvf $HOME/0g-storage-node/run/db/flow_db.tar.gz -C $HOME/0g-storage-node/run/db/
 
 echo ""
 echo "Restart Your Service/Node"
@@ -20,3 +20,4 @@ sudo systemctl restart zgs
 
 echo ""
 echo "✔️Done: Your Node Will start Syncing From Block 3507655 🚀"
+
